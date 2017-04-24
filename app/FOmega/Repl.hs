f27020caa88adeb7c464960a9cfe66c3fe2f284d
Bree Elle Gardner <letsbreelhere@gmail.com>
2017-04-24 15:07:13 -0700

    REPL now evaluates when displaying result

--- app/FOmega/Repl.hs
+++ app/FOmega/Repl.hs
@@ -1,10 +1,13 @@
 module Main where
 
 import Util.Repl
-import FOmega.TypeCheck
+import FOmega.Eval
 import FOmega.Parsing
+import FOmega.TypeCheck
 
+main :: IO ()
 main = repl "ω> " $ \input -> pure . either id id $ do
   parsed <- parseTerm input
   ty <- typeCheck [] [] parsed
-  pure $ show parsed ++ " has type " ++ show ty
+  let down = eval parsed
+  pure $ show down ++ " has type " ++ show ty
